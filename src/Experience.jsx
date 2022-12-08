494a231780d74ff0a61bdab29bae1be72088f59a
Maiya Winter <wintermaiya@gmail.com>
2022-12-08 10:01:04 -0700

    Updating Colors

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,42 +6,42 @@ import Stars from "./galaxy/Stars";
 const Experience = () => {
 	// All items to be rendered within the THree.js canvas.
 	const galaxyMainProperties = {
-		count: 1030000,
+		count: 730000,
 		size: 0.05,
 		radius: 9,
 		branches: 4,
 		spin: 0.6,
 		randomness: 0.3,
 		randomnessPower: 1.5,
-		insideColor: "#DE3163",
+		insideColor: "#FE8966",
 		outsideColor: "#52c8ff",
-		rotation: 0.005,
+		rotation: 0.001,
 	};
 
 	const galaxyCenterProperties = {
-		count: 300000,
+		count: 90000,
 		size: 0.03,
 		radius: 3,
 		branches: 3,
 		spin: 100,
 		randomness: 0.6,
 		randomnessPower: 3,
-		insideColor: "#ff6030",
-		outsideColor: "#DE3163",
-		rotation: 0.01,
+		insideColor: "#FE8966",
+		outsideColor: "#52c8ff",
+		rotation: 0.007,
 	};
 
 	const galaxyCenterOuter = {
-		count: 500000,
+		count: 200000,
 		size: 0.05,
 		radius: 6,
 		branches: 3,
 		spin: 100,
 		randomness: 0.5,
 		randomnessPower: 3,
-		insideColor: "#ff6030",
-		outsideColor: "#E37383",
-		rotation: 0.007,
+		insideColor: "#FFE6E1",
+		outsideColor: "#52c8ff",
+		rotation: 0.003,
 	};
 
 	const starsProperties = {
--- src/galaxy/BlackHole.jsx
+++ src/galaxy/BlackHole.jsx
@@ -2,7 +2,7 @@ import { Sphere } from "@react-three/drei";
 
 const BlackHole = () => {
 	return (
-		<Sphere scale={0.3}>
+		<Sphere scale={0.1}>
 			<meshBasicMaterial color="black" />
 		</Sphere>
 	);
