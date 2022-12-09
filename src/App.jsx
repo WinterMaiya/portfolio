da946c3093f2d4415cf2bcac39d2bc1af62f54e5
Maiya Winter <wintermaiya@gmail.com>
2022-12-09 13:13:01 -0700

    Removing App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 import { Environment, PerformanceMonitor, Stats } from "@react-three/drei";
