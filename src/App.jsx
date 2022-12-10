dae63cb4a8f0e337d13075588cd8c072f90833b9
Maiya Winter <wintermaiya@gmail.com>
2022-12-10 12:26:15 -0700

    Adding Custom Camera for Controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
-import { PerformanceMonitor, Stats } from "@react-three/drei";
+import {
+	PerformanceMonitor,
+	PerspectiveCamera,
+	Stats,
+} from "@react-three/drei";
 import Interface from "./Interface";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
 	// Rendering the application
 	// Canvas refers to the 3D canvas while Interface refers to the HTML ui above.
 
-	// [X, Y, Z]
-	const [cameraLocation, setCameraLocation] = useState([22, 36, 22]);
+	// Reference for the PerspectiveCamera
+	const cameraRef = useRef();
+
 	return (
 		<>
-			<Canvas
-				camera={{
-					fov: 60,
-					near: 0.1,
-					far: 20000,
-					position: cameraLocation,
-				}}
-			>
+			<Canvas>
+				<PerspectiveCamera
+					makeDefault
+					position={[22, 36, 22]}
+					ref={cameraRef}
+				/>
 				<Stats />
 				<PerformanceMonitor />
 				<Experience />
 			</Canvas>
-			<Interface setCameraLocation={setCameraLocation} />
+			<Interface cameraRef={cameraRef} />
 		</>
 	);
 }
--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,11 +1,20 @@
-const Interface = ({ setCameraLocation }) => {
+const Interface = ({ cameraRef }) => {
 	// Creates a interface that goes above the 3D canvas.
 	// Uses Material Ui for the styling
 	return (
 		<div className="interface">
 			<div className="centered">
 				<h1>Maiya Winter</h1>
-				<button>Explore</button>
+				<button
+					onClick={() => {
+						console.log(cameraRef.current.position);
+						cameraRef.current.position.x = 5;
+						cameraRef.current.position.y = 5;
+						cameraRef.current.position.z = 5;
+					}}
+				>
+					Explore
+				</button>
 			</div>
 		</div>
 	);
