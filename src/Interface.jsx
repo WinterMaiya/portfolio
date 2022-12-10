f2290721386ae6ab6f0982cd8f95b55c1607957b
Maiya Winter <wintermaiya@gmail.com>
2022-12-10 13:33:07 -0700

    Updating Sections

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -7,7 +7,7 @@ const Interface = ({ cameraRef }) => {
 	// Creates a interface that goes above the 3D canvas.
 	// Uses Material Ui for the styling
 
-	// "loading", "welcome", "home"
+	// "loading", "welcome", "home", "projects", "art", "about", "contact"
 	const [displayState, setDisplayState] = useState("welcome");
 
 	const animate = (newX, newY, newZ, state) => {
@@ -29,77 +29,75 @@ const Interface = ({ cameraRef }) => {
 	};
 	return (
 		<div className="interface">
-			<section id="welcome" hidden={displayState === "welcome" ? false : true}>
-				<Grid
-					container
-					spacing={0}
-					direction="column"
-					alignItems="center"
-					justifyContent="center"
-					style={{ minHeight: "100vh" }}
-					zIndex="99"
-					position="absolute"
+			<Container>
+				<section
+					id="welcome"
+					hidden={displayState === "welcome" ? false : true}
 				>
-					<Grid item margin={5}>
-						<Zoom in={displayState === "welcome"}>
-							<Typography
-								variant="h1"
-								fontSize="8vw"
-								color="white"
-								align="center"
-							>
-								Maiya Winter's Portfolio
-							</Typography>
-						</Zoom>
+					<Grid
+						container
+						spacing={0}
+						direction="column"
+						alignItems="center"
+						justifyContent="center"
+						style={{ minHeight: "100vh" }}
+						zIndex="99"
+					>
+						<Grid item margin={5}>
+							<Zoom in={displayState === "welcome"}>
+								<Typography
+									variant="h1"
+									fontSize="8vw"
+									color="white"
+									align="center"
+								>
+									Maiya Winter's Universe
+								</Typography>
+							</Zoom>
+						</Grid>
+						<Grid item xs={3} margin={5}>
+							<Zoom in={displayState === "welcome"}>
+								<Button
+									id="welcome-button"
+									onClick={() => {
+										animate(2, 4, -9, "home");
+										setDisplayState("loading");
+									}}
+									variant="contained"
+									size="large"
+								>
+									Enter
+								</Button>
+							</Zoom>
+						</Grid>
 					</Grid>
-					<Grid item xs={3} margin={5}>
-						<Zoom in={displayState === "welcome"}>
-							<Button
-								id="welcome-button"
-								onClick={() => {
-									animate(2, 4, -9, "home");
-									setDisplayState("loading");
-								}}
-								variant="contained"
-								size="large"
-							>
-								Enter
-							</Button>
-						</Zoom>
-					</Grid>
-				</Grid>
-			</section>
-			<section id="home" hidden={displayState === "home" ? false : true}>
-				<Grid
-					container
-					spacing={0}
-					direction="column"
-					alignItems="center"
-					justifyContent="center"
-					style={{ minHeight: "100vh" }}
-					zIndex="99"
-					position="absolute"
-				>
-					<Grid item xs={3} margin={5}>
-						<Typography variant="h1" color="primary">
-							This is the home page now
-						</Typography>
-					</Grid>
-					<Grid item xs={3} margin={50}>
-						<Button
-							id="welcome-button"
-							onClick={() => {
-								animate(2, 4, -9, "home");
-								setDisplayState("loading");
-							}}
-							variant="contained"
-							size="large"
-						>
-							Enter
-						</Button>
-					</Grid>
-				</Grid>
-			</section>
+				</section>
+				<section id="home" hidden={displayState === "home" ? false : true}>
+					<Grid
+						container
+						spacing={0}
+						direction="column"
+						alignItems="center"
+						justifyContent="center"
+						style={{ minHeight: "100vh" }}
+						zIndex="99"
+						position="absolute"
+					></Grid>
+				</section>
+				<section id="welcome" hidden={displayState === "home" ? false : true}>
+					<Grid
+						container
+						spacing={0}
+						direction="column"
+						alignItems="center"
+						justifyContent="center"
+						style={{ minHeight: "100vh" }}
+						zIndex="99"
+						position="absolute"
+					></Grid>
+				</section>
+			</Container>
+
 			{/* <div className="centered">
 				<h1>Maiya Winter</h1>
 				<button
